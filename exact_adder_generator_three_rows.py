47efe780c74a8d447b4c2f01b3fc94a778a718ec
jmforsythe <72832948+jmforsythe@users.noreply.github.com>
2020-07-23 00:55:10 +0100

    Added script to generate chromosome of an exact adder using three rows

---
+++ exact_adder_generator_three_rows.py
@@ -0,0 +1,36 @@
+input_lengths = 8
+
+chromosome = []
+
+if input_lengths == 1:
+    chromosome = [[0, 1, "XOR"], [0, 1, "AND"], [0, 0, "XOR"], 3, 2]
+else:
+    for i in range(input_lengths - 1):
+        if i == 0:
+            chromosome.append([input_lengths - 1, 2 * input_lengths - 1, "XOR"])
+            chromosome.append([input_lengths - 1, 2 * input_lengths - 1, "AND"])
+            chromosome.append([input_lengths - 2, 2 * input_lengths - 2, "XOR"])
+            chromosome.append([2 * input_lengths + 1, 2 * input_lengths + 2, "AND"])
+            chromosome.append([2 * input_lengths + 1, 2 * input_lengths + 2, "XOR"])
+            chromosome.append([input_lengths - 2, 2 * input_lengths - 2, "AND"])
+        else:
+            chromosome.append([2 * input_lengths + 6 * i - 3, 2 * input_lengths + 6 * i - 1, "OR"])
+            chromosome.append([input_lengths - i - 2, 2 * input_lengths - i - 2, "XOR"])
+            chromosome.append([0, 0, "XOR"])
+            chromosome.append([2 * input_lengths + 6 * i, 2 * input_lengths + 6 * i + 1, "AND"])
+            chromosome.append([2 * input_lengths + 6 * i, 2 * input_lengths + 6 * i + 1, "XOR"])
+            chromosome.append([input_lengths - i - 2, 2 * input_lengths - i - 2, "AND"])
+
+    chromosome.append([2 * input_lengths + 6 * (input_lengths - 1) - 3, 2 * input_lengths + 6 * (input_lengths - 1) - 1, "OR"])
+    chromosome.append([0, 0, "XOR"])
+    chromosome.append([0, 0, "XOR"])
+
+    length = len(chromosome)
+    chromosome.append(2 * input_lengths + length - 3)
+    for i in range(input_lengths - 1):
+        chromosome.append(length + 2 * input_lengths - 5 - i * 6)
+    chromosome.append(2 * input_lengths)
+
+print(chromosome)
+
+        
